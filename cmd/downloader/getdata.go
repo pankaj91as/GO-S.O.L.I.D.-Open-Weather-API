663c345a50c7a112568ac1c250c3c43befa22ec5
Pankaj Pawar <pankaj@Pankajs-MacBook-Air.local>
2024-04-24 15:57:51 +0530

    update & insert data

--- cmd/downloader/getdata.go
+++ cmd/downloader/getdata.go
@@ -7,6 +7,8 @@ import (
 	"log"
 	"net/http"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type OpenWeatherAPI struct{}
@@ -33,7 +35,28 @@ func (ll *OpenWeatherAPI) FetchData(latitude float64, longitude float64) {
 		fmt.Println(err)
 	}
 
-	weatherData := WeatherDataHistory{
+	weatherHistoryData := WeatherDataHistory{
+		Lon:           APIResponse["coord"].(map[string]interface{})["lon"].(float64),
+		Lat:           APIResponse["coord"].(map[string]interface{})["lat"].(float64),
+		MainTemp:      APIResponse["main"].(map[string]interface{})["temp"].(float64),
+		MainTempMin:   APIResponse["main"].(map[string]interface{})["temp_min"].(float64),
+		MainTempMax:   APIResponse["main"].(map[string]interface{})["temp_max"].(float64),
+		MainPressure:  int(APIResponse["main"].(map[string]interface{})["pressure"].(float64)),
+		MainFeelsLike: APIResponse["main"].(map[string]interface{})["feels_like"].(float64),
+		MainHumidity:  int(APIResponse["main"].(map[string]interface{})["humidity"].(float64)),
+		WindSpeed:     APIResponse["wind"].(map[string]interface{})["speed"].(float64),
+		WindDeg:       int(APIResponse["wind"].(map[string]interface{})["deg"].(float64)),
+		CloudsAll:     int(APIResponse["clouds"].(map[string]interface{})["all"].(float64)),
+		SysCountry:    APIResponse["sys"].(map[string]interface{})["country"].(string),
+		SysSunrise:    time.Unix(int64(APIResponse["sys"].(map[string]interface{})["sunrise"].(float64))+int64(APIResponse["timezone"].(float64)), 0),
+		SysSunset:     time.Unix(int64(APIResponse["sys"].(map[string]interface{})["sunset"].(float64))+int64(APIResponse["timezone"].(float64)), 0),
+		Name:          APIResponse["name"].(string),
+		Base:          APIResponse["base"].(string),
+		Visibility:    int(APIResponse["visibility"].(float64)),
+		Dt:            time.Unix(int64(APIResponse["dt"].(float64)), 0),
+	}
+
+	weatherCurrentData := WeatherData{
 		Lon:           APIResponse["coord"].(map[string]interface{})["lon"].(float64),
 		Lat:           APIResponse["coord"].(map[string]interface{})["lat"].(float64),
 		MainTemp:      APIResponse["main"].(map[string]interface{})["temp"].(float64),
@@ -66,22 +89,40 @@ func (ll *OpenWeatherAPI) FetchData(latitude float64, longitude float64) {
 	defer dbConn.Close()
 
 	// Verify Table Exist
-	var weatherTable []WeatherDataHistory
-	table_check := ormdb.First(&weatherTable)
+	var weatherHistoryTable []WeatherDataHistory
+	var weatherTable []WeatherData
+	historyTableCheck, weatherTableCheck := ormdb.First(&weatherHistoryTable), ormdb.First(&weatherTable)
 
-	if table_check == nil {
-		fmt.Println("table is there")
-	} else {
-		err = ormdb.AutoMigrate(&WeatherDataHistory{})
-		if err != nil {
-			log.Fatalf("Error auto-migrating schema: %v", err)
-		}
+	if historyTableCheck != nil {
+		migrateTable(ormdb, &weatherHistoryTable)
+	}
+
+	if weatherTableCheck != nil {
+		migrateTable(ormdb, &weatherTable)
 	}
 
 	// Insert Data into Weather Data History Table
-	result := ormdb.Create(&weatherData)
-	if result.Error != nil {
-		fmt.Println(result.Error)
+	insertData := ormdb.Create(&weatherHistoryData)
+	if insertData.Error != nil {
+		fmt.Println(insertData.Error)
 	}
 
+	var weatherSearch = WeatherData{Lat: weatherHistoryData.Lat, Lon: weatherHistoryData.Lon}
+	findResult := ormdb.First(&weatherSearch)
+	if findResult.RowsAffected == 0 {
+		findResult = ormdb.Create(&weatherCurrentData)
+	} else {
+		findResult = ormdb.Model(&WeatherData{}).Where("Lat = ?", weatherHistoryData.Lat).Where("Lon = ?", weatherHistoryData.Lon).Updates(&weatherCurrentData)
+	}
+
+	if findResult.Error != nil {
+		fmt.Println(findResult.Error)
+	}
+}
+
+func migrateTable(ormdb *gorm.DB, ns interface{}) {
+	err := ormdb.AutoMigrate(ns)
+	if err != nil {
+		log.Fatalf("Error auto-migrating schema: %v", err)
+	}
 }
--- cmd/downloader/weather.go
+++ cmd/downloader/weather.go
@@ -26,6 +26,30 @@ type WeatherDataHistory struct {
 	UpdatedAt     time.Time
 }
 
+type WeatherData struct {
+	ID            uint    `gorm:"primaryKey"`
+	Lon           float64 `gorm:"index" json:"Coord.Lon"`
+	Lat           float64 `gorm:"index" json:"Coord.Lat"`
+	MainTemp      float64 `json:"Main.Temp"`
+	MainTempMin   float64 `json:"Main.TempMin"`
+	MainTempMax   float64 `json:"Main.TempMax"`
+	MainPressure  int     `json:"Main.Pressure"`
+	MainFeelsLike float64 `json:"Main.FeelsLike"`
+	MainHumidity  int     `json:"Main.Humidity"`
+	WindSpeed     float64
+	WindDeg       int
+	CloudsAll     int
+	SysCountry    string
+	SysSunrise    time.Time
+	SysSunset     time.Time
+	Name          string `gorm:"index"`
+	Base          string
+	Visibility    int
+	Dt            time.Time
+	CreatedAt     time.Time
+	UpdatedAt     time.Time
+}
+
 type WeatherAPIResponse struct {
 	Coord      Coord   `json:"coord"`
 	Weather    Weather `json:"weather"`
